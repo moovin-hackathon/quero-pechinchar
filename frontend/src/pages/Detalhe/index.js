74ee411c05b5f6ceda2f5b06b765c22ed72a36c6
Marcos Willian <markusuuuu@gmail.com>
2019-10-20 04:07:45 -0300

    feature modal/share

--- frontend/src/pages/Detalhe/index.js
+++ frontend/src/pages/Detalhe/index.js
@@ -17,10 +17,16 @@ import {
   ButtonContainer,
   DescriptionTitle,
   DescriptionText,
-  FeaturedImageContainer
+  FeaturedImageContainer,
+  ContainerModal,
+  DescriptionCont,
+  TextShare,
+  ShareTime,
+  ButtonShare
 } from './styles';
 
 import Loading from '../../components/Loading';
+import Modal from '@material-ui/core/Modal';
 
 import photo1 from '../../images/product/foto1.png';
 import photo2 from '../../images/product/foto2.png';
@@ -28,14 +34,35 @@ import photo3 from '../../images/product/foto3.png';
 import photo4 from '../../images/product/foto4.png';
 
 import { formatPrice } from '../../utils/format';
+
 export default class Detalhe extends Component {
   state = {
+    ModalOpen: false,
+    compartilhado: false,
+    modalOpen: false,
     product: {
       descricao: '',
       nome: '',
       preco: null,
     },
   };
+
+  handleModalOpen = () => {
+    this.setState({
+      ModalOpen: true,
+    })
+  }
+
+  handleModalClose = () => {
+    this.setState({
+      ModalOpen: false,
+    });
+    
+  }
+
+  handleBuyModal = () => {
+   return alert('oi');
+  }
 
   componentDidMount() {
     let ref = Firebase.database().ref('/Produto');
@@ -53,7 +80,7 @@ export default class Detalhe extends Component {
   }
 
   render() {
-    const { product } = this.state;
+    const { product, ModalOpen, compartilhado } = this.state;
     console.log(product);
     return (
       <Container>
@@ -75,7 +102,7 @@ export default class Detalhe extends Component {
               <ProductPrice>{product.preco ? product.preco : <Loading width="130px" />}</ProductPrice>
               <ButtonContainer>
                 <BuyButton>Comprar</BuyButton>
-                <ShareWithFacebook>Compartilhar</ShareWithFacebook>
+                <ShareWithFacebook onClick={compartilhado ? this.handleBuyModal : this.handleModalOpen}>{compartilhado ? 'Comprar' : 'Pechinchar'}</ShareWithFacebook>
               </ButtonContainer>
               <DescriptionContainer>
                 <DescriptionTitle>Descrição</DescriptionTitle>
@@ -83,6 +110,23 @@ export default class Detalhe extends Component {
               </DescriptionContainer>
             </ProductDescriptionContainer>
           </ProductContainer>
+          {ModalOpen && (
+            <Modal open={ModalOpen} onClose={this.handleModalClose}>
+              <ContainerModal>
+                <DescriptionCont>
+                  <TextShare>divulgue o produto nas suas redes sociais e ganhe desconto na sua compra quanto mais acessos pelo seu link maior o seu desconto.</TextShare>
+                  <ShareTime>O tempo máximo para pechinchar o desconto será de (tempo) horas.</ShareTime>
+                </DescriptionCont>
+                <>
+                  <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="large">
+                    <ButtonShare onClick={() => this.setState({ compartilhado: true })}>
+                      <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartilhar</a>
+                    </ButtonShare>
+                  </div>
+                </>
+              </ContainerModal>
+            </Modal>
+          )}
       </Container>
     )
   }
