a35fbcf28c1e4f1d3e074e922d70316f62da25ce
Ander Robson <ander.robson1287@gmail.com>
2019-10-20 00:54:02 -0300

    Realiza alteração na Landing page

--- frontend/src/pages/Landing/index.js
+++ frontend/src/pages/Landing/index.js
@@ -16,234 +16,204 @@ import { Box } from '@material-ui/core'
 import Logo from '../../images/logo-stack-underflow.jpg'
 
 const styles = theme => ({
-  main: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  root: {
-    flexGrow: 1,
-    flex: '1 0 100%'
-  },
-  hero: {
-    height: '100%',
-    flex: '0 0 auto',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: theme.palette.background.paper,
-    color: theme.palette.type === 'light' ? theme.palette.primary.dark : theme.palette.primary.main
-  },
-  text: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: theme.spacing(4)
-  },
-  title: {
-    letterSpacing: '.7rem',
-    textIndent: '.7rem',
-    fontWeight: theme.typography.fontWeightLight,
-    [theme.breakpoints.only('xs')]: {
-      fontSize: 24,
-      letterSpacing: '.1em',
-      textIndent: '.1rem'
+    main: {
+        display: 'flex',
+        flexDirection: 'column'
     },
-    whiteSpace: 'nowrap'
-  },
-  h5: {
-    paddingLeft: theme.spacing(1) * 4,
-    paddingRight: theme.spacing(1) * 4,
-    marginTop: theme.spacing(1),
-    maxWidth: 600,
-    textAlign: 'center',
-    [theme.breakpoints.only('xs')]: {
-      fontSize: 18
+    root: {
+        flexGrow: 1,
+        flex: '1 0 100%'
+    },
+    hero: {
+        height: '100%',
+        flex: '0 0 auto',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.palette.background.paper,
+        color: theme.palette.primary.main
+    },
+    text: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: theme.spacing(4)
+    },
+    title: {
+        letterSpacing: '.7rem',
+        textIndent: '.7rem',
+        fontWeight: theme.typography.fontWeightLight,
+        [theme.breakpoints.only('xs')]: {
+            fontSize: 24,
+            letterSpacing: '.1em',
+            textIndent: '.1rem'
+        },
+        whiteSpace: 'nowrap'
+    },
+    h5: {
+        paddingLeft: theme.spacing(1) * 4,
+        paddingRight: theme.spacing(1) * 4,
+        marginTop: theme.spacing(1),
+        maxWidth: 600,
+        textAlign: 'center',
+        [theme.breakpoints.only('xs')]: {
+            fontSize: 18
+        }
+    },
+    content: {
+        height: '100%',
+        [theme.breakpoints.up('sm')]: {
+            paddingTop: theme.spacing(1)
+        }
+    },
+    button: {
+        marginTop: theme.spacing(1) * 3
+    },
+    logo: {
+        margin: `${theme.spacing(1) * 3}px 0 ${theme.spacing(1) * 4}px`,
+    },
+    steps: {
+        maxWidth: theme.spacing(1) * 130,
+        margin: 'auto'
+    },
+    step: {
+        padding: `${theme.spacing(1) * 3}px ${theme.spacing(1) * 2}px`
+    },
+    stepIcon: {
+        marginBottom: theme.spacing(1)
+    },
+    markdownElement: {},
+    cardsContent: {
+        padding: 15,
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexWrap: 'wrap',
+        [theme.breakpoints.only('xs')]: {
+            width: '100%',
+            padding: 0,
+            paddingTop: 15
+        }
+    },
+    card: {
+        minWidth: 275,
+        maxWidth: 450,
+        margin: 15,
+        [theme.breakpoints.only('xs')]: {
+            width: '100%',
+            margin: 0,
+            marginTop: 7
+        }
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)'
+    },
+    cardTitle: {
+        marginBottom: 16,
+        fontSize: 14
+    },
+    pos: {
+        marginBottom: 12
     }
-  },
-  content: {
-    height: '100%',
-    [theme.breakpoints.up('sm')]: {
-      paddingTop: theme.spacing(1)
-    }
-  },
-  button: {
-    marginTop: theme.spacing(1) * 3
-  },
-  logo: {
-    margin: `${theme.spacing(1) * 3}px 0 ${theme.spacing(1) * 4}px`,
-  },
-  steps: {
-    maxWidth: theme.spacing(1) * 130,
-    margin: 'auto'
-  },
-  step: {
-    padding: `${theme.spacing(1) * 3}px ${theme.spacing(1) * 2}px`
-  },
-  stepIcon: {
-    marginBottom: theme.spacing(1)
-  },
-  markdownElement: {},
-  cardsContent: {
-    padding: 15,
-    display: 'flex',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap',
-    [theme.breakpoints.only('xs')]: {
-      width: '100%',
-      padding: 0,
-      paddingTop: 15
-    }
-  },
-  card: {
-    minWidth: 275,
-    maxWidth: 350,
-    margin: 15,
-    [theme.breakpoints.only('xs')]: {
-      width: '100%',
-      margin: 0,
-      marginTop: 7
-    }
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
-  cardTitle: {
-    marginBottom: 16,
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
 })
 
 const LandingPage = ({ classes, history, theme }) => {
-  const isAuthorised = () => {
-    try {
-      const key = Object.keys(localStorage).find(e => e.match(/persist:root/))
-      const data = JSON.parse(localStorage.getItem(key))
-      const auth = JSON.parse(data.auth)
 
-      return auth && auth.isAuthorised
-    } catch (ex) {
-      return false
-    }
-  }
-
-  useEffect(() => {
-    if (isAuthorised()) {
-      history.push('/signin')
-    }
-  })
-
-  return (
-    <div className={classes.main}>
-      <Helmet>
+    return (
+        <div className={classes.main}>
+        <Helmet>
         <meta name="theme-color" content={theme.palette.primary.main} />
-        <meta name="apple-mobile-web-app-status-bar-style" content={theme.palette.primary.main} />
-        <meta name="msapplication-navbutton-color" content={theme.palette.primary.main} />
-        <title>Moovinfy</title>
-      </Helmet>
-      <AppBar position="static">
+    <meta name="apple-mobile-web-app-status-bar-style" content={theme.palette.primary.main} />
+    <meta name="msapplication-navbutton-color" content={theme.palette.primary.main} />
+    <title>Quero Pechinchar</title>
+    </Helmet>
+    <AppBar position="static">
         <Toolbar disableGutters>
-          <div style={{ flex: 1 }} />
-
-          <Tooltip id="tooltip-icon1" title="Sign in">
-            <IconButton
-              name="signin"
-              aria-label="Open login"
-              color="inherit"
-              onClick={() => {
-                history.push('/signin')
-              }}
-              rel="noopener"
-            >
-              <LockIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip id="tooltip-icon2" title="GitHub repository">
-            <IconButton
-              name="github"
-              aria-label="Open Github"
-              color="inherit"
-              href="https://bitbucket.org/moovin/moovinfy"
-              target="_blank"
-              rel="noopener"
-            >
-              <GitHubIcon />
-            </IconButton>
-          </Tooltip>
+    <div style={{ flex: 1 }} />
+    <Tooltip id="tooltip-icon2" title="GitHub repository">
+        <IconButton
+    name="github"
+    aria-label="Open Github"
+    color="inherit"
+    href="https://github.com/moovin-hackathon/quero-pechinchar"
+    target="_blank"
+    rel="noopener"
+        >
+        <GitHubIcon />
+        </IconButton>
+        </Tooltip>
         </Toolbar>
-      </AppBar>
+        </AppBar>
 
-      <div className={classes.root}>
+        <div className={classes.root}>
         <div className={classes.hero}>
-          <div className={classes.content}>
-            <Box display="flex" justifyContent="center">
-              <img src={Logo} alt="Material-UI Logo" className={classes.logo} />
-            </Box>
-            <div className={classes.text}>
-              <Typography
-                variant="h3"
-                align="center"
-                component="h1"
-                color="inherit"
-                gutterBottom
-                className={classes.title}
-              >
-                {'SEJA BEM VINDO AO HACKATHON!'}
-              </Typography>
-              <Typography variant="h5" component="h2" color="inherit" gutterBottom className={classes.h5}>
-                {'Tema: Alavancando vendas através dos canais digitais.'}
-              </Typography>
-              <Button
-                onClick={() => {
-                  history.push('/signin')
-                }}
-                className={classes.button}
-                variant="outlined"
-                color="primary"
-              >
-                {'Comece agora!'}
-              </Button>
-            </div>
-
-            <div className={classes.cardsContent}>
-              <Card className={classes.card}>
-                <CardContent>
-                  <Typography variant="h5" component="h2">
-                    Documente
-                  </Typography>
-                  <br />
-                  <Typography>{'Compartilhe códigos de diversas linguagens de desenvolvedor para desenvolvedor'}</Typography>
-                </CardContent>
-              </Card>
-              <Card className={classes.card}>
-                <CardContent>
-                  <Typography variant="h5" component="h2">
-                    Pesquise
-                  </Typography>
-                  <br />
-                  <Typography>{'Precisa de algo? Busque por projetos da empresa que contêm o que você necessita'}</Typography>
-                </CardContent>
-              </Card>
-              <Card className={classes.card}>
-                <CardContent>
-                  <Typography variant="h5" component="h2">
-                    Achou um bug?
-                  </Typography>
-                  <br />
-                  <Typography>{'Relate em nosso sistema de resolução de problemas'}</Typography>
-                </CardContent>
-              </Card>
-            </div>
-          </div>
+        <div className={classes.content}>
+        <Box display="flex" justifyContent="center">
+        <img src={Logo} alt="Material-UI Logo" className={classes.logo} />
+    </Box>
+    <div className={classes.text}>
+        <Typography
+    variant="h3"
+    align="center"
+    component="h1"
+    color="inherit"
+    gutterBottom
+    className={classes.title}
+        >
+        {'SEJA BEM VINDO AO QUERO PECHINCHAR!'}
+        </Typography>
+        <Typography variant="h5" component="h2" color="inherit" gutterBottom className={classes.h5}>
+        {'Tema: Alavancando vendas através dos canais digitais.'}
+        </Typography>
+        <Button
+    onClick={() => {
+        history.push('/product')
+    }}
+    className={classes.button}
+    variant="outlined"
+    color="primary"
+        >
+        {'Comece agora!'}
+        </Button>
         </div>
-      </div>
+
+        <div className={classes.cardsContent}>
+        <Card className={classes.card}>
+        <CardContent>
+        <Typography variant="h5" component="h2">
+        O que é?
+</Typography>
+    <br />
+    <Typography>{'Quero Pechinchar é um módulo em forma de campanha para alavancar as vendas de seu e-commerce de um modo mais dinâmico.'}</Typography>
+    </CardContent>
+    </Card>
+    <Card className={classes.card}>
+        <CardContent>
+        <Typography variant="h5" component="h2">
+        Qual o diferencial?
+</Typography>
+    <br />
+    <Typography>{'Influencia a utilização de redes sociais como modo de divulgação. Os consumidores recebem descontos dependendo do quanto o produto foi compartilhado'}</Typography>
+    </CardContent>
+    </Card>
+    <Card className={classes.card}>
+        <CardContent>
+        <Typography variant="h5" component="h2">
+        Nosso stack
+    </Typography>
+    <br />
+    <Typography>{'[Firebase] (https://firebase.google.com/) [React](https://github.com/facebookincubator/create-react-app) [Material-UI](https://material-ui.com/)'}</Typography>
+    </CardContent>
+    </Card>
     </div>
-  )
+
+    </div>
+    </div>
+    </div>
+    </div>
+)
 }
 
 export default withRouter(withStyles(styles, { withTheme: true })(LandingPage))
