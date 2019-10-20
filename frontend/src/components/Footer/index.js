436104f1968761fc09d45982d378d32bfd527a21
EltonH <elhay22@gmail.com>
2019-10-20 08:10:02 -0300

    hotfixes

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,7 +5,7 @@ import { Container } from './styles';
 export default function Footer() {
   return (
     <Container>
-      <p>2019 - Copyrigth todos os direitos reservados<br />Quero Pechinchar</p>
+      <p>2019 - &copy; Copyright todos os direitos reservados<br />Quero Pechinchar</p>
     </Container>
   );
 }
